b4cff5bf9ef703b25d641096d086bf628c1bce33
EoLima <lucasl.ima@icloud.com>
2025-08-22 17:42:05 -0300

    feat: add booking content

---
+++ app/_constants/search.ts
@@ -0,0 +1,20 @@
+
+interface QuickSearchOption {
+  imageUrl: string;
+  title: string;
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/Razor.png",
+    title: "Cabelo"
+  },
+  {
+    imageUrl: "/Mustache.png",
+    title: "Barba"
+  },
+  {
+    imageUrl: "/Scissors.png",
+    title: "Acabamento"
+  }
+]
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({ orderBy: { name: "desc" } })
 
   return (
     <div>
@@ -24,6 +25,14 @@ const Home = async () => {
             <SearchIcon />
           </Button>
         </div>
+        <div className=" mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions?.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image src={option.imageUrl} alt={option.title} width={16} height={16} />
+              {option.title}
+            </Button>
+          ))}
+        </div>
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/BannerPizza.png"
@@ -32,33 +41,25 @@ const Home = async () => {
             className="rounded-xl object-cover"
           />
         </div>
+        <BookingItem />
         <h2 className="text-xs font-bold uppercase text-gray-400 mt-6 mb-3">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia Souza</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
-         <h2 className="text-xs font-bold uppercase text-gray-400 mt-6 mb-3">
           Recomendados
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">{barbershops.map(barbershop => <BarbershopItem barbershop={barbershop} key={barbershop.id} />)}</div>
+        <h2 className="text-xs font-bold uppercase text-gray-400 mt-6 mb-3">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">{popularBarbershops.map(barbershop => <BarbershopItem barbershop={barbershop} key={barbershop.id} />)}</div>
       </div>
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6 ">
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">Fluxo Soluções Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
